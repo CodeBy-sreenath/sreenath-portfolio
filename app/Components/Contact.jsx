a272e83f47cd93c0b66a11469673b0ced050aaaa
CodeBy-sreenath <sreenathts655@gmail.com>
2026-03-06 09:34:46 -0800

    contact session was added

---
+++ app/Components/Contact.jsx
@@ -0,0 +1,37 @@
+export default function Contact() {
+  return (
+    <section id="contact" className="p-10 bg-gray-100">
+
+      <h2 className="text-3xl font-bold mb-4">
+        Contact
+      </h2>
+
+      <p>Email: sreenathts655@gmail.com</p>
+
+      <p>Phone: 9778373317</p>
+
+      <p>
+        GitHub:
+        <a
+          className="text-blue-500"
+          href="https://github.com/CodeBy-sreenath"
+          target="_blank"
+        >
+          github.com/CodeBy-sreenath
+        </a>
+      </p>
+
+      <p>
+        LinkedIn:
+        <a
+          className="text-blue-500"
+          href="https://www.linkedin.com/in/sreenath-ts-b33442362"
+          target="_blank"
+        >
+          LinkedIn Profile
+        </a>
+      </p>
+
+    </section>
+  );
+}
