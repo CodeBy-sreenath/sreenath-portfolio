56aee68946dc86dfd402b8c6a85c5e13e206c353
CodeBy-sreenath <sreenathts655@gmail.com>
2026-03-06 09:31:47 -0800

    project added

---
+++ data/projects.jsx
@@ -0,0 +1,38 @@
+const projects = [
+  {
+    title: "Result Analysis System",
+    description:
+      "AI powered MERN stack student result management system that analyzes results and calculates pass percentage.",
+    link: "https://result-analyis-c8rb.vercel.app/",
+  },
+
+  {
+    title: "YATHRA Bus Booking",
+    description:
+      "Bus seat booking system with secure payment using Stripe.",
+    link: "https://bus-ticket-dnz6.vercel.app/",
+  },
+
+  {
+    title: "AI Farmer",
+    description:
+      "AI powered crop disease detection system using leaf images with confidence score and solutions.",
+    link: "https://ai-farmer-wsqz.vercel.app/",
+  },
+
+  {
+    title: "QuickGrieve",
+    description:
+      "Mobile app for submitting civic complaints. AI prioritizes complaints using NLP.",
+    link: "#",
+  },
+
+  {
+    title: "Zar TradeLinks",
+    description:
+      "Company website developed using Next.js.",
+    link: "https://zar-tradelinks.vercel.app/",
+  },
+];
+
+export default projects;
